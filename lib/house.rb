e5de4e2db03c3b3b53c11551a1775895e2a29226
boykj <jboyk918@gmail.com>
2021-04-15 10:24:29 -0700

    house.rb: Add default_verse func to reduce future duplication

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,14 @@
 class House
 
     def line(number = :FIXME)
+
+        def default_verse
+            "This is the house that Jack built.\n"
+        end
+
         case number
         when 1
-            "This is the house that Jack built.\n"
+            "#{default_verse}"
         when 2
             "This is the malt that lay in the house that Jack built.\n"
         when 3
