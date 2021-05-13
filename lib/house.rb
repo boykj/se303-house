5b2bff39a6ba503de3b988c9d9f50fa3dd292517
boykj <jboyk918@gmail.com>
2021-05-12 18:13:33 -0700

    house.rb: replace 3 clause with default

--- lib/house.rb
+++ lib/house.rb
@@ -16,7 +16,7 @@ class House
 	when 2
 	  "This is the malt that lay #{default}.\n"
 	when 3
-	  "This is the rat that ate the malt that lay in the house that Jack built.\n"
+	  "This is the rat that ate the malt that lay #{default}.\n"
 	when 4
 	  "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 	when 5
