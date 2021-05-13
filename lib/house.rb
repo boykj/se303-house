e165f49c51f3617ca90bef6825f0fc998aec6367
boykj <jboyk918@gmail.com>
2021-05-12 18:15:38 -0700

    house.rb: replace 7 clause with default

--- lib/house.rb
+++ lib/house.rb
@@ -24,7 +24,7 @@ class House
 	when 6
 	  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 7
-	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 8
 	  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 	when 9
