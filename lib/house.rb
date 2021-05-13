9519d1e9dcf0606e1d7d76a74aeb23fd397fb3b9
boykj <jboyk918@gmail.com>
2021-05-12 19:40:30 -0700

    house.rb: add 2 clause for additional_verse: remove 2 clause from phase method in place of changes

--- lib/house.rb
+++ lib/house.rb
@@ -9,8 +9,6 @@ class House
 		case number
 		when 1
 			"#{additional_verse(number)}"
-		when 2
-			"This is the malt #{default}.\n"
 		else
 			"#{intro} #{additional_verse(number)} #{default}.\n"
 		end
@@ -32,6 +30,8 @@ class House
 		case number
 		when 1
 			"This is the house that Jack built.\n"
+		when 2
+			"the malt"
 		when 3
 			"the rat that ate the malt"
 		when 4
