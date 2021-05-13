c90c4e681b26e61849d1fd009cd4ce8c3e5bf487
boykj <jboyk918@gmail.com>
2021-05-12 18:13:00 -0700

    house.rb: Add default method: commit error?

--- lib/house.rb
+++ lib/house.rb
@@ -1,41 +1,49 @@
 class House
 
   def recite
-    all_lines(12, 1)
+    recite_all_lines(1, 12)
+		#(1..12).collect { |i| line(i)}.join("\n")
   end
 
-  def all_lines(upper, lower)
+  def recite_all_lines(lower, upper)
     lower.upto(upper).collect { |i| line(i) }.join("\n")
   end
     
-  def line(number = :FIXME)
-
-  	case number
-    	when 1
-    	  "This is the house that Jack built.\n"
-    	when 2
-    	  "This is the malt that lay in the house that Jack built.\n"
-    	when 3
-    	  "This is the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 4
-    	  "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 5
-    	  "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 6
-    	  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 7
-    	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 8
-    	  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 9
-    	  "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 10
-    	  "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 11
-    	  "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    	when 12
-    	  "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+  def phrase(number = :FIXME)
+	case number
+	when 1
+	  "This is the house that Jack built.\n"
+	when 2
+	  "This is the malt that lay #{default}.\n"
+	when 3
+	  "This is the rat that ate the malt that lay in the house that Jack built.\n"
+	when 4
+	  "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 5
+	  "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 6
+	  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 7
+	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 8
+	  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 9
+	  "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 10
+	  "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 11
+	  "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+	when 12
+	  "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
   	end
+  end
+
+  def line(number)
+	"#{phrase(number)}"
+  end
+
+  def default
+	return "in the house that Jack built"
   end
 
 end
