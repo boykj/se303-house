fa18f57b8ea20ebf39888461b7c8918062158218
boykj <jboyk918@gmail.com>
2021-05-12 18:40:37 -0700

    house.rb: revert previous changes to default method: Remove if clause for the 1 case to go in a different direction

--- lib/house.rb
+++ lib/house.rb
@@ -1,35 +1,36 @@
 class House
 
   def recite
-	(1..12).collect { |i| line(i)}.join("\n")
+		(1..12).collect { |i| line(i)}.join("\n")
   end
+
     
   def phrase(number = :FIXME)
 	case number
 	when 1
-	  "#{base_verse(number)}\n"
+	  "This is the house that Jack built.\n"
 	when 2
-	  "This is the malt #{base_verse}.\n"
+	  "This is the malt #{default}.\n"
 	when 3
-	  "This is the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the rat that ate the malt #{default}.\n"
 	when 4
-	  "This is the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the cat that killed the rat that ate the malt #{default}.\n"
 	when 5
-	  "This is the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 6
-	  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 7
-	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 8
-	  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 9
-	  "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 10
-	  "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 11
-	  "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
 	when 12
-	  "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{malt_verse} #{base_verse}.\n"
+	  "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt #{default}.\n"
   	end
   end
 
@@ -37,16 +38,8 @@ class House
 	"#{phrase(number)}"
   end
 
-  def base_verse(number = :FIXME)
-		if number == 1
-			"This is the house that Jack built."
-		else
-			"that lay in the house that Jack built"
-		end
+  def default
+		"that lay in the house that Jack built"
   end
-
-	def malt_verse
-		"that ate the malt"
-	end
 
 end
