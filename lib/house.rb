9243ae51b332f9628e5643c3c3c58e8bffccb39e
boykj <jboyk918@gmail.com>
2021-05-12 18:49:52 -0700

    house.rb: changes to additional_verse method: include 'the rat' because of common occurences

--- lib/house.rb
+++ lib/house.rb
@@ -12,38 +12,38 @@ class House
 	when 2
 	  "This is the malt #{default}.\n"
 	when 3
-	  "This is the rat #{additional_verse} #{default}.\n"
+	  "This is #{additional_verse} #{default}.\n"
 	when 4
-	  "This is the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the cat that killed #{additional_verse} #{default}.\n"
 	when 5
-	  "This is the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the dog that worried the cat that killed #{additional_verse} #{default}.\n"
 	when 6
-	  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed #{additional_verse} #{default}.\n"
 	when 7
-	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{additional_verse} #{default}.\n"
 	when 8
-	  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{additional_verse} #{default}.\n"
 	when 9
-	  "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{additional_verse} #{default}.\n"
 	when 10
-	  "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{additional_verse} #{default}.\n"
 	when 11
-	  "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{additional_verse} #{default}.\n"
 	when 12
-	  "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat #{additional_verse} #{default}.\n"
+	  "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed #{additional_verse} #{default}.\n"
   	end
   end
 
   def line(number)
-	"#{phrase(number)}"
+		"#{phrase(number)}"
   end
 
   def default
 		"that lay in the house that Jack built"
   end
 
-	def additional_verse
-		"that ate the malt"
+	def additional_verse(number = :FIXME)
+		"the rat that ate the malt"
 	end
 
 end
