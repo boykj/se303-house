a9cc89329d1b94ef962e9a395fbacc2ba1bd9e26
boykj <jboyk918@gmail.com>
2021-05-18 10:26:10 -0700

    house.rb: remove full verse from array: only add 'the house' implemenent to case 1

--- lib/house.rb
+++ lib/house.rb
@@ -26,15 +26,11 @@ class House
 		"This is"
 	end
 
-	ary = ["This is |the horse and the hound and the horn that belonged to |the farmer sowing his corn that kept 
-	|the rooster that crowed in the morn that woke |the priest all shaven and shorn that married |the man all tattered and torn that kissed 
-	|the maiden all forlorn that milked |the cow with the crumpled horn that tossed |the dog that worried |the cat that killed |the rat that ate 
-	|the malt |that lay in |the house |that Jack built."]
-
 	def additional_verse(number)
+		ary = ["the house"]
 		case number
 		when 1
-			"the house"
+			"#{ary[0]}"
 		when 2
 			"the malt that lay"
 		when 3
