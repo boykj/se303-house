37d1625280c3ae130c3df9d1bc3c2ea60a2637c2
boykj <jboyk918@gmail.com>
2021-05-18 12:36:03 -0700

    house.rb: rename additional_verse to verses: also rename verses array to rename_me

--- lib/house.rb
+++ lib/house.rb
@@ -5,24 +5,24 @@ class House
   end
 
   def line(number)
-		"This is #{additional_verse(number)}.\n"
+		"This is #{verses(number)}.\n"
   end
 
-	def additional_verse(number)
+	def verses(number)
 
-		verses = ["the house", "the malt that lay in", "the rat that ate", "the cat that killed",
+		rename_me = ["the house", "the malt that lay in", "the rat that ate", "the cat that killed",
 			"the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked",
 			"the man all tattered and torn that kissed", "the priest all shaven and shorn that married",
 			"the rooster that crowed in the morn that woke", "the farmer sowing his corn that kept",
-			"the horse and the hound and the horn that belonged to", "that jack built"
+			"the horse and the hound and the horn that belonged to"
 		]
 
 		if number == 1
-			"#{verses[number - 1]}" + " that Jack built"
+			"#{rename_me[number - 1]}" + " that Jack built"
 		elsif number == 2
-			"#{verses[number - 1]}" + " the house that Jack built"
+			"#{rename_me[number - 1]}" + " the house that Jack built"
 		else
-			"#{verses[number - 1]} " + additional_verse(number - 1)
+			"#{rename_me[number - 1]} " + verses(number - 1)
 		end
 	end
 
