784ed915342fb3ebbc314970b465deec4cd45e6f
boykj <jboyk918@gmail.com>
2021-05-18 12:47:29 -0700

    house.rb: change base array verse: 'the house that Jack built

--- lib/house.rb
+++ lib/house.rb
@@ -10,7 +10,7 @@ class House
 
 	def verses(number)
 
-		rename_me = ["the house", "the malt that lay in", "the rat that ate", "the cat that killed",
+		rename_me = ["the house that Jack built", "the malt that lay in", "the rat that ate", "the cat that killed",
 			"the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked",
 			"the man all tattered and torn that kissed", "the priest all shaven and shorn that married",
 			"the rooster that crowed in the morn that woke", "the farmer sowing his corn that kept",
@@ -18,9 +18,7 @@ class House
 		]
 
 		if number == 1
-			"#{rename_me[number - 1]}" + " that Jack built"
-		elsif number == 2
-			"#{rename_me[number - 1]}" + " the house that Jack built"
+			"#{rename_me[number - 1]}" 
 		else
 			"#{rename_me[number - 1]} " + verses(number - 1)
 		end
