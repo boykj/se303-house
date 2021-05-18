0b899d72e0213dd6d8117dc2cf441995c22f52ca
boykj <jboyk918@gmail.com>
2021-05-18 10:44:58 -0700

    house.rb: add rooster verse to ary

--- lib/house.rb
+++ lib/house.rb
@@ -30,7 +30,8 @@ class House
 
 		ary = ["the house", "the malt that lay", "the rat that ate", "the cat that killed",
 			"the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked",
-			"the man all tattered and torn that kissed", "the priest all shaven and shorn that married"
+			"the man all tattered and torn that kissed", "the priest all shaven and shorn that married",
+			"the rooster that crowed in the morn that woke"
 		]
 
 		case number
