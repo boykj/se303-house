e0c0878461cf831b552f6d8f583e2b2d90915e6b
boykj <jboyk918@gmail.com>
2021-05-18 12:20:23 -0700

    house.rb: add base_verses to array

--- lib/house.rb
+++ lib/house.rb
@@ -28,13 +28,13 @@ class House
 			"the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked",
 			"the man all tattered and torn that kissed", "the priest all shaven and shorn that married",
 			"the rooster that crowed in the morn that woke", "the farmer sowing his corn that kept",
-			"the horse and the hound and the horn that belonged to"
+			"the horse and the hound and the horn that belonged to", "in the house", "that jack built"
 		]
 
 		if number == 1 || number == 2
-			"#{ary[number_minus_one]}"
+			"#{ary[number - 1]}"
 		else
-			"#{ary[number_minus_one]} " + additional_verse(number_minus_one)
+			"#{ary[number - 1]} " + additional_verse(number - 1)
 		end
 	end
 
