b2d0479f22025902887c7a36f52c1da038874efe
boykj <jboyk918@gmail.com>
2021-05-30 18:45:07 -0700

    house.rb: add predecessor method: not sure if the right direction or not

--- lib/house.rb
+++ lib/house.rb
@@ -13,13 +13,13 @@ end
 
 class HouseVerse
 
-	#attr_reader :number, :pronouns
+	attr_reader :number
 
 	def initialize(number)
 		@number = number
 	end
 
-	def pronoun_method
+	def pronouns_method
 		return pronouns = ["the house that Jack built", "the malt that lay in", "the rat that ate", "the cat that killed",
 		"the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked",
 		"the man all tattered and torn that kissed", "the priest all shaven and shorn that married",
@@ -27,12 +27,16 @@ class HouseVerse
 		"the horse and the hound and the horn that belonged to"
 	]
 	end
+	
+	def predecessor(number)
+		number - 1
+	end
 
 	def verses(number)
 		if number == 1
-			"#{pronoun_method[number - 1]}" 
+			"#{pronouns_method[predecessor(number)]}"
 		else
-			"#{pronoun_method[number - 1]} " + verses(number - 1)
+			"#{pronouns_method[predecessor(number)]} " + verses(number - 1)
 		end
 	end
 	
