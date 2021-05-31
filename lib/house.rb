6bf80e60211d88a5c2288c67dadca4a321e009cf
boykj <jboyk918@gmail.com>
2021-05-30 18:20:19 -0700

    house.rb: copy verses method to HouseVerse class

--- lib/house.rb
+++ lib/house.rb
@@ -27,5 +27,23 @@ class House
 end
 
 class HouseVerse
+
 	attr_reader :number
+
+	def verses(number)
+
+		pronouns = ["the house that Jack built", "the malt that lay in", "the rat that ate", "the cat that killed",
+			"the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked",
+			"the man all tattered and torn that kissed", "the priest all shaven and shorn that married",
+			"the rooster that crowed in the morn that woke", "the farmer sowing his corn that kept",
+			"the horse and the hound and the horn that belonged to"
+		]
+
+		if number == 1
+			"#{pronouns[number - 1]}" 
+		else
+			"#{pronouns[number - 1]} " + verses(number - 1)
+		end
+	end
+	
 end
