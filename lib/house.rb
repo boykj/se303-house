2c1306c89badd9159b51bb37e5bad829044f371d
boykj <jboyk918@gmail.com>
2021-05-30 18:32:26 -0700

    house.rb: remove pronouns array from verses method

--- lib/house.rb
+++ lib/house.rb
@@ -29,14 +29,6 @@ class HouseVerse
 	end
 
 	def verses(number)
-
-		pronouns = ["the house that Jack built", "the malt that lay in", "the rat that ate", "the cat that killed",
-			"the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked",
-			"the man all tattered and torn that kissed", "the priest all shaven and shorn that married",
-			"the rooster that crowed in the morn that woke", "the farmer sowing his corn that kept",
-			"the horse and the hound and the horn that belonged to"
-		]
-
 		if number == 1
 			"#{pronoun_method[number - 1]}" 
 		else
